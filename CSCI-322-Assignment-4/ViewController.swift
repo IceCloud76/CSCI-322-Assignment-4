fae555880b82f7173a991d15d1522ff5513a05f5
Eric <starwolf64.exe@gmail.com>
2021-03-15 21:49:42 -0500

    Initial Commit

---
+++ CSCI-322-Assignment-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSCI-322-Assignment-4
+//
+//  Created by Eric Kirchman on 3/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
