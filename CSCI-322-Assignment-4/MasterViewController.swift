a23eed4be1350317a43f83c6eb5a6c70e96e3a4e
Eric <starwolf64.exe@gmail.com>
2021-03-17 16:06:46 -0500

    Fixed PresidentCell. It was a swift file, but I deleted and created a cocoa file

--- CSCI-322-Assignment-4/MasterViewController.swift
+++ CSCI-322-Assignment-4/MasterViewController.swift
@@ -10,13 +10,52 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var presidents: [USPresident] = []
+    //var presidents: [USPresident] = []
+    var presidents = ["Barack Obama", "Joe Biden", "Donald Trump"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        /*
+        loadPropertyList()
+        presidents.sort {
+            $0.name < $1.name
+        }
+        */
     }
 
+    /*
+    func loadPropertyList() {
+        guard let path = Bundle.main.path(forResource: "presidents", ofType: "plist"), let xml = FileManager.default.contents(atPath: path) else {
+            fatalError("Unable to access property list presidents.plist")
+        }
+        
+        do {
+            presidents = try PropertyListDecoder().decode([USPresident].self, from: xml)
+        } catch {
+            fatalError("Unable to decode property list characters.plist")
+        }
+    }
+    */
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presidents.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PresidentCell
+
+        // Configure the cell...
+        //cell.characterImageView.image = UIImage(named: presidents[indexPath.row].name)
+        cell.nameLabel?.text = presidents[indexPath.row].name
+        cell.allegianceLabel?.text = presidents[indexPath.row].allegiance
+
+        return cell
+    }
     
 }
--- CSCI-322-Assignment-4/PresidentCell.swift
+++ CSCI-322-Assignment-4/PresidentCell.swift
@@ -4,25 +4,25 @@
 //
 //  Created by Eric Kirchman on 3/17/21.
 //
-// PresidentCell = CharacterCell
-
-import Foundation
 
 import UIKit
 
 class PresidentCell: UITableViewCell {
+
     
-/*
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var politicalPartyLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-*/
-/*
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-*/
+
 }
