6a3f7257ce80fb761c5a5f507d15ce5ec05d3a8b
Eric <starwolf64.exe@gmail.com>
2021-03-19 16:33:52 -0500

    Added Launchscreen

--- CSCI-322-Assignment-4/DetailViewController.swift
+++ CSCI-322-Assignment-4/DetailViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  CSCI-322-Assignment-4
 //
-//  Created by Eric Kirchman on 3/15/21.
+//  Created by Eric Kirchman Z1864162 and Dan Hufford Z1864126 on 3/15/21.
 //
 
 import UIKit
--- CSCI-322-Assignment-4/USPresident.swift
+++ CSCI-322-Assignment-4/USPresident.swift
@@ -2,7 +2,7 @@
 //  USPresident.swift
 //  CSCI-322-Assignment-4
 //
-//  Created by Eric Kirchman on 3/17/21.
+//  Created by Eric Kirchman Z1864162 and Dan Hufford Z1864126 on 3/17/21.
 //
 // USPresident = MCUCharacter
 
